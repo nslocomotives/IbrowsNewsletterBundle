5ddf7f9d396ace28b3884df3f98a187468ad100d
Mike Meier <sk.mykon@gmail.com>
2012-10-18 17:56:46 +0200

    docs

--- Model/Newsletter/Newsletter.php
+++ Model/Newsletter/Newsletter.php
@@ -9,44 +9,72 @@ class Newsletter implements NewsletterInterface
 	protected $senderName;
 	protected $returnMail;
 
+    /**
+     * @return string
+     */
 	public function getSubject()
 	{
 	    return $this->subject;
 	}
 
+    /**
+     * @param string $subject
+     * @return Newsletter
+     */
 	public function setSubject($subject)
 	{
 	    $this->subject = $subject;
 	    return $this;
 	}
 
+    /**
+     * @return string
+     */
 	public function getSenderMail()
 	{
 	    return $this->senderMail;
 	}
 
+    /**
+     * @param string $senderMail
+     * @return Newsletter
+     */
 	public function setSenderMail($senderMail)
 	{
 	    $this->senderMail = $senderMail;
 	    return $this;
 	}
 
+    /**
+     * @return string
+     */
 	public function getSenderName()
 	{
 	    return $this->senderName;
 	}
 
+    /**
+     * @param string $senderName
+     * @return Newsletter
+     */
 	public function setSenderName($senderName)
 	{
 	    $this->senderName = $senderName;
 	    return $this;
 	}
 
+    /**
+     * @return string
+     */
 	public function getReturnMail()
 	{
 	    return $this->returnMail;
 	}
 
+    /**
+     * @param type $returnMail
+     * @return Newsletter
+     */
 	public function setReturnMail($returnMail)
 	{
 	    $this->returnMail = $returnMail;
