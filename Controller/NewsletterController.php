1e5f56fad1f2a7852b0fe9e9c967b222ebdce425
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-18 15:06:36 +0200

    newsletter controller additions

--- Controller/NewsletterController.php
+++ Controller/NewsletterController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ibrows\Bundle\NewsletterBundle\Controller;
 
+use Ibrows\Bundle\NewsletterBundle\Form\NewsletterFormType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -8,47 +10,100 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class NewsletterController extends Controller
 {
 	/**
-	 * @Route("/", name="ibrows_newsletter_index")
-	 * @Template()
+	 * @Route("/{client}/", defaults={"client" = "default"}, name="ibrows_newsletter_index")
 	 */
-	public function indexAction()
+	public function indexAction($client)
 	{
+		$templates = $this->get('ibrows_newsletter.template_manager');
+		$client = $this->get('ibrows_newsletter.client_manager')->get($client);
+		$newsletters = $client->getNewsletters();
 		
+		return $this->render($templates->getNewsletter('index'), array(
+				'newsletters' => $newsletters
+		));
 	}
 	
 	/**
-	 * @Route("/create", name="ibrows_newsletter_create")
-	 * @Template()
+	 * @Route("/{client}/create", defaults={"client" = "default"}, name="ibrows_newsletter_create")
 	 */
-	public function createAction()
+	public function createAction($client)
 	{
+		$templates = $this->get('ibrows_newsletter.template_manager');
+		$classes = $this->get('ibrows_newsletter.class_manager');
+
+		$client = $this->get('ibrows_newsletter.client_manager')->get($client);
+		$newsletter = $client->createNewsletter();
 		
+		$formtype = $classes->getForm('newsletter_meta');
+		$form = $this->createForm(new $formtype(), $newsletter);
+		
+		$request = $this->getRequest();
+		if ($request->getMethod() == 'POST') {
+			$form->bindRequest($request);
+			
+			if ($form->isValid()) {
+				$client->persist($newsletter);
+				
+				return $this->redirect($this->generateUrl('ibrows_newsletter_edit', array(
+						'client' => $client->getName(),
+						'id' => $newsletter->getId(),
+				)));
+			}
+		}
+	
+		return $this->render($templates->getNewsletter('create'), array(
+				'newsletter' => $newsletter,
+				'client' => $client->getName(),
+				'form' => $form->createView(),
+		));
 	}
 	
 	/**
-	 * @Route("/{id}/edit", name="ibrows_newsletter_edit")
-	 * @Template()
+	 * @Route("/{client}/edit/{id}", defaults={"client" = "default"}, name="ibrows_newsletter_edit")
 	 */
-	public function editAction()
+	public function editAction($client, $id)
 	{
+		$templates = $this->get('ibrows_newsletter.template_manager');
+		$classes = $this->get('ibrows_newsletter.class_manager');
+
+		$object = $this->get('ibrows_newsletter.client_manager')->get($client);
+		$newsletter = $object->createNewsletter();
 		
+		$formtype = $classes->getForm('newsletter_content');
+		$form = $this->createForm(new $formtype(), $newsletter);
+	
+		return $this->render($templates->getNewsletter('edit'), array(
+				'newsletter' => $newsletter,
+				'client' => $client,
+				'form' => $form->createView(),
+		));
 	}
 	
 	/**
-	 * @Route("/{id}/send", name="ibrows_newsletter_send")
-	 * @Template()
+	 * @Route("/{client}/send/{id}", defaults={"client" = "default"}, name="ibrows_newsletter_send")
 	 */
-	public function sendAction()
+	public function sendAction($client, $id)
 	{
+		$templates = $this->get('ibrows_newsletter.template_manager');
+		$client = $this->get('ibrows_newsletter.client_manager')->get($client);
+		$newsletter = $client->getNewsletter($id);
 		
+		return $this->render($templates->getNewsletter('send'), array(
+				'newsletter' => $newsletter
+		));
 	}
 	
 	/**
-	 * @Route("/{id}/summary", name="ibrows_newsletter_summary")
-	 * @Template()
+	 * @Route("/{client}/summary/{id}", defaults={"client" = "default"}, name="ibrows_newsletter_summary")
 	 */
-	public function summaryAction()
+	public function summaryAction($client, $id)
 	{
+		$templates = $this->get('ibrows_newsletter.template_manager');
+		$client = $this->get('ibrows_newsletter.client_manager')->get($client);
+		$newsletter = $client->getNewsletter($id);
 		
+		return $this->render($templates->getNewsletter('summary'), array(
+				'newsletter' => $newsletter
+		));
 	}
 }
