b7418c9eb6e48a97041771caaaab6dc7617db3fa
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-18 15:04:38 +0200

    newsletter additions

--- Model/Newsletter.php
+++ Model/Newsletter.php
@@ -4,5 +4,52 @@ namespace Ibrows\Bundle\NewsletterBundle\Model;
 
 class Newsletter implements NewsletterInterface
 {
+	protected $subject;
+	protected $sender_mail;
+	protected $sender_name;
+	protected $return_mail;
 
+	public function getSubject()
+	{
+	    return $this->subject;
+	}
+
+	public function setSubject($subject)
+	{
+	    $this->subject = $subject;
+	    return $this;
+	}
+
+	public function getSenderMail()
+	{
+	    return $this->sender_mail;
+	}
+
+	public function setSenderMail($sender_mail)
+	{
+	    $this->sender_mail = $sender_mail;
+	    return $this;
+	}
+
+	public function getSenderName()
+	{
+	    return $this->sender_name;
+	}
+
+	public function setSenderName($sender_name)
+	{
+	    $this->sender_name = $sender_name;
+	    return $this;
+	}
+
+	public function getReturnMail()
+	{
+	    return $this->return_mail;
+	}
+
+	public function setReturnMail($return_mail)
+	{
+	    $this->return_mail = $return_mail;
+	    return $this;
+	}
 }
--- Model/NewsletterInterface.php
+++ Model/NewsletterInterface.php
@@ -4,5 +4,8 @@ namespace Ibrows\Bundle\NewsletterBundle\Model;
 
 interface NewsletterInterface
 {
-
+	public function getSubject();
+	public function getSenderMail();
+	public function getSenderName();
+	public function getReturnMail();
 }
