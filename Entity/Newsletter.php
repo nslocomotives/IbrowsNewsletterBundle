89f60bcd0024251a18cec637c634a10d24c506f3
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-18 15:06:12 +0200

    newsletter persistence for orm

--- Entity/Newsletter.php
+++ Entity/Newsletter.php
@@ -1,10 +1,24 @@
 <?php
-
 namespace Ibrows\Bundle\NewsletterBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Ibrows\Bundle\NewsletterBundle\Model\Newsletter as AbstractNewsletter;
 
 class Newsletter extends AbstractNewsletter
 {
-
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $subject;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $sender_mail;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $sender_name;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $return_mail;
 }
