58ce33156de583371ac0566a3390becad46860c6
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-22 19:25:29 +0200

    newsletter manager

---
+++ Service/orm/NewsletterManager.php
@@ -0,0 +1,25 @@
+<?php
+namespace Ibrows\Bundle\NewsletterBundle\Service\orm;
+
+use Ibrows\Bundle\NewsletterBundle\Model\Newsletter\NewsletterManagerInterface;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+
+use Ibrows\Bundle\NewsletterBundle\Model\Newsletter\NewsletterManager as BaseNewsletterManager;
+
+class NewsletterManager extends BaseNewsletterManager
+{
+	protected $repository;
+	
+	public function __construct(ObjectRepository $repository)
+	{
+		$this->repository = $repository;
+		parent::__construct($repository->getClassName());
+	}
+	
+	public function get($id)
+	{
+		return $this->repository->find($id);
+	}
+
+}
