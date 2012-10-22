f29807b7eb7e1fed1ae964bbbc3e3e0222d91cbb
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-22 18:37:50 +0200

    persistNewsletter in mandant manager interface (not nice but ok for now)

--- Model/Mandant/MandantManagerInterface.php
+++ Model/Mandant/MandantManagerInterface.php
@@ -2,21 +2,30 @@
 
 namespace Ibrows\Bundle\NewsletterBundle\Model\Mandant;
 
+use Ibrows\Bundle\NewsletterBundle\Model\Newsletter\NewsletterInterface;
+
 interface MandantManagerInterface
 {
 	const DEFAULT_NAME = 'default';
 	
 	/**
-	 * 
-	 * @param unknown_type $name
+	 * @param string $name
 	 */
 	public function get($name);
 	/**
-	 * 
 	 * @param string $name
 	 * @return \Symfony\Component\Security\Core\User\UserProviderInterface
 	 */
 	public function getUserProvider($name);
-	
+	/**
+	 * @param string $name
+	 * @return \Ibrows\Bundle\NewsletterBundle\Model\Newsletter\NewsletterManagerInterface
+	 */
 	public function getNewsletterManager($name);
+	/**
+	 * @param string $name
+	 * @param NewsletterInterface $newsletter
+	 * @return NewsletterInterface|null
+	 */
+	public function persistNewsletter($name, NewsletterInterface $newsletter);
 }
