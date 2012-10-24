b7f0b691a85553c77db140003a329811c2c567b3
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-24 10:51:49 +0200

    login definitions

---
+++ Security/Token/MandantUsernamePasswordToken.php
@@ -0,0 +1,36 @@
+<?php
+namespace Ibrows\Bundle\NewsletterBundle\Security\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class MandantUsernamePasswordToken extends UsernamePasswordToken
+{
+	const LAST_USERNAME = SecurityContextInterface::LAST_USERNAME;
+	const LAST_MANDANT = '_security.last_mandant';
+	
+	protected $mandant;
+	
+	public function __construct($user, $credentials, $mandant, $providerKey, array $roles = array())
+	{
+		$this->mandant = $mandant;
+		
+		parent::__construct($user, $credentials, $providerKey, $roles);
+	}
+	
+	public function getMandant()
+	{
+		return $this->mandant;
+	}
+	
+	public function serialize()
+	{
+		return serialize(array($this->mandant, parent::serialize()));
+	}
+	
+	public function unserialize($str)
+	{
+		list($this->mandant, $parentStr) = unserialize($str);
+		parent::unserialize($parentStr);
+	}
+}
