e8e4c31108dce10d41ae915488dac16dc770ecfa
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-24 15:41:46 +0200

    twig renderer should not use existing twig engine

--- Renderer/TwigRenderer.php
+++ Renderer/TwigRenderer.php
@@ -7,19 +7,16 @@ use Symfony\Bridge\Twig\TwigEngine;
 class TwigRenderer implements RendererInterface
 {
 	protected $engine;
-	protected $origloader;
-	protected $stringloader;
 	
-	public function __construct(\Twig_Environment $engine)
+	public function __construct()
 	{
-		$this->engine = $engine;
-		$this->origloader = $engine->getLoader();
-		$this->stringloader = new \Twig_Loader_String();
+		$this->engine = new \Twig_Environment();
+		$stringloader = new \Twig_Loader_String();
+		$this->engine->setLoader($stringloader);
 	}
 
 	public function render(RenderableInterface $element, array $parameters = array())
 	{
-		$this->engine->setLoader($this->stringloader);
 		try {
 			$rendered = $this->engine->render($element->getContent(), $parameters);
 			
@@ -27,7 +24,6 @@ class TwigRenderer implements RendererInterface
 			$rendered = $e->getMessage();
 		}
 		
-		$this->engine->setLoader($this->origloader);
 		return $rendered;
 	}
 
