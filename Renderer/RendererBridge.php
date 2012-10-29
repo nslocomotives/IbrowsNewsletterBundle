3b86aff049c00266f97bd14a88e0aa436e1c51f1
Mike Meier <sk.mykon@gmail.com>
2012-10-29 09:30:48 +0100

    format

--- Renderer/RendererBridge.php
+++ Renderer/RendererBridge.php
@@ -3,33 +3,47 @@
 namespace Ibrows\Bundle\NewsletterBundle\Renderer;
 
 use Ibrows\Bundle\NewsletterBundle\Model\Subscriber\SubscriberInterface;
-use Symfony\Component\Routing\RouterInterface;
+use Ibrows\Bundle\NewsletterBundle\Renderer\GenderTitleStrategy\GenderTitleStrategyInterface;
+use Ibrows\Bundle\NewsletterBundle\Model\Newsletter\NewsletterInterface;
 
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 class RendererBridge
 {
-    protected $translator;
     protected $router;
+    protected $genderTitleStrategy;
     
-    public function __construct(TranslatorInterface $translator, RouterInterface $router)
-    {
-        $this->translator = $translator;
+    public function __construct(
+        RouterInterface $router,
+        GenderTitleStrategyInterface $genderTitleStrategy
+    ){
         $this->router = $router;
+        $this->genderTitleStrategy = $genderTitleStrategy;
     }
     
-    public function getNow()
+    public function getNow(SubscriberInterface $subscriber, $format = 'd.m.Y')
     {
-        
+        $currentLocale = setlocale(LC_TIME, 0);
+        setlocale(LC_TIME, $subscriber->getLocale());
+        $now = strftime($format);
+        setlocale(LC_TIME, $currentLocale);
+        return $now;
     }
     
-    public function getUnsubscribeLink(SubscriberInterface $subscriber)
+    public function getUnsubscribeLink(NewsletterInterface $newsletter, SubscriberInterface $subscriber)
     {
-        return '<a href="">unsubscribe</a>';
+        return $this->router->generate(
+            'ibrows_newsletter_unsubscribe', 
+            array(
+                'newsletter' => $newsletter->getHash(), 
+                'subscriber' => $subscriber->getHash()
+            ), 
+            true
+        );
     }
     
-    public function getTitle(SubscriberInterface $subscriber)
+    public function getGenderTitle(SubscriberInterface $subscriber)
     {
-        return 'i dont know your title';
+        return $this->genderTitleStrategy->getGenderTitle($subscriber);
     }
 }
