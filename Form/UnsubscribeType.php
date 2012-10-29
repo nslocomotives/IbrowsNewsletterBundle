91d57d6c548492e1c4ad262a424c15fb7f0b7a0b
Mike Meier <sk.mykon@gmail.com>
2012-10-29 17:42:05 +0100

    add subscriber controller with list view implemented unsubscribe added iframe auto resize

---
+++ Form/UnsubscribeType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ibrows\Bundle\NewsletterBundle\Form;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityChoiceList;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UnsubscribeType extends AbstractType
+{
+    protected $managerName;
+    protected $groupClass;
+
+    public function __construct($managerName, $groupClass)
+    {
+        $this->managerName = $managerName;
+        $this->groupClass = $groupClass;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('groups', 'entity', array(
+                'em' => $this->managerName,
+                'class' => $this->groupClass,
+                'expanded' => true,
+                'multiple' => true,
+            ))
+        ;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'ibrows_newsletterbundle_unsubscribe';
+    }
+
+}
