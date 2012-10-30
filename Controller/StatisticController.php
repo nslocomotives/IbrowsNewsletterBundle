0094aab752903610144b08aa06af0016987772b1
Mike Meier <sk.mykon@gmail.com>
2012-10-30 15:57:30 +0100

    added mandanthash

--- Controller/StatisticController.php
+++ Controller/StatisticController.php
@@ -5,16 +5,29 @@ namespace Ibrows\Bundle\NewsletterBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @Route("/statistic")
  */
-class StatisticController extends AbstractController
+class StatisticController extends AbstractHashMandantController
 {
-    /**
-     * @Route("/log/{newsletterHash}/{subscriberHash}", name="ibrows_newsletter_statistic_log")
-     */
-    public function logAction($newsletterHash, $subscriberHash)
-    {
+    const TRANSPARENT_GIF = 'R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
+    /**
+     * @Route("/log/read/{mandantHash}/{newsletterHash}/{subscriberHash}", name="ibrows_newsletter_statistic_log_read")
+     */
+    public function logreadAction($mandantHash, $newsletterHash, $subscriberHash)
+    {
+        $this->setMandantNameByHash($mandantHash);
+
+        // if no context is set, its live --> log
+        if(!$this->getRequest()->get('context')){
+
+        }
+
+        return new Response(base64_decode(self::TRANSPARENT_GIF), 200, array(
+            'Content-Type' => 'image/gif'
+        ));
     }
 }
