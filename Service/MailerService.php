8a4d1217af7ed59ad89e0f3e34a11fc45b19e4a7
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-31 13:18:46 +0100

    mailer service definition

---
+++ Service/MailerService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ibrows\Bundle\NewsletterBundle\Service;
+
+use Ibrows\Bundle\NewsletterBundle\Model\Job\MailJob;
+
+class MailerService
+{
+	protected $mailer;
+	
+	public function __construct($mailer)
+	{
+		$this->mailer = $mailer;
+	}
+	
+	public function send(MailJob $job)
+	{
+		$message = \Swift_Message::newInstance()
+			->setSubject($job->getSubject())
+			->setFrom($job->getSenderMail(), $job->getSenderName())
+			->setReturnPath($job->getReturnMail())
+			->setTo($job->getToMail())
+			->setBody($job->getBody())
+		;
+		
+		$this->mailer->setHost($job->getHost());
+		$this->mailer->setPort($job->getPort());
+		$this->mailer->setUsername($job->getUsername());
+		$this->mailer->setPassword($job->getPassword());
+		
+		$this->mailer->send($message);
+	}
+	
+}
