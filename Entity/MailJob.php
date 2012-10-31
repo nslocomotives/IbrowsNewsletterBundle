5c3046e8271a075290df5b80a62c6005362071d9
Dominik Spengler <dominik.spengler@ibrows.ch>
2012-10-31 11:02:52 +0100

    mail job definitions

---
+++ Entity/MailJob.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ibrows\Bundle\NewsletterBundle\Entity;
+
+use Ibrows\Bundle\NewsletterBundle\Model\Job\MailJob as BaseMailJob;
+use Doctrine\ORM\Mapping as ORM;
+
+class MailJob extends BaseMailJob
+{
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $subject;
+	/**
+	 * @ORM\Column(type="string", name ="sender_name")
+	 */
+	protected $senderName;
+	/**
+	 * @ORM\Column(type="string", name ="sender_mail")
+	 */
+	protected $senderMail;
+	/**
+	 * @ORM\Column(type="string", name ="return_mail")
+	 */
+	protected $returnMail;
+	
+	/**
+	 * @ORM\Column(type="string", name ="to_mail")
+	 */
+	protected $toMail;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $body;
+	
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $error;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $status;
+	
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	protected $created;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	protected $scheduled;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	protected $completed;
+}
---
+++ Model/Job/AbstractJob.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Ibrows\Bundle\NewsletterBundle\Model\Job;
+
+abstract class AbstractJob implements JobInterface
+{
+	protected $error;
+	protected $status;
+	
+	protected $created;
+	protected $scheduled;
+	protected $completed;
+	
+	public function __construct()
+	{
+		$this->created = new \DateTime();
+	}
+
+	public function getError()
+	{
+		return $this->error;
+	}
+	
+	public function setError($error)
+	{
+		$this->error = $error;
+		return $this;
+	}
+	
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    
+    public function getCreated()
+    {
+    		return $this->created;
+    }
+    
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function getScheduled()
+    {
+        return $this->scheduled;
+    }
+
+    public function setScheduled(\DateTime $scheduled)
+    {
+        $this->scheduled = $scheduled;
+        return $this;
+    }
+
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(\DateTime $completed)
+    {
+        $this->completed = $completed;
+        return $this;
+    }
+
+}
