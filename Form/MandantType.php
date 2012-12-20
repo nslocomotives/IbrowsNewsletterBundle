370d17636bca9654843018bf09a1cd4025db052b
Mike Meier <mike.meier@ibrows.ch>
2012-12-20 11:24:15 +0100

    allow changing the renderer name

--- Form/MandantType.php
+++ Form/MandantType.php
@@ -13,7 +13,7 @@ class MandantType extends AbstractType
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
-			->add('name')
+			->add('rendererName')
 		;
 	}
 	
