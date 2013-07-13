9e4e4505c0071d4dc9fa1126ed16f4c27f463b09
Francesco Casula <fra.casula@gmail.com>
2013-07-13 11:35:34 +0200

    new delete option on subscribers list

--- Controller/SubscriberController.php
+++ Controller/SubscriberController.php
@@ -4,6 +4,7 @@ namespace Ibrows\Bundle\NewsletterBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/subscriber")
@@ -21,4 +22,28 @@ class SubscriberController extends AbstractController
             'subscribers' => $subscribers
         ));
     }
+
+	/**
+	 * @Route("/delete/{id}", requirements={"id" = "\d+"}, name="ibrows_newsletter_subscriber_delete")
+	 */
+	public function deleteAction($id)
+	{
+		$subManager = $this->getMandantManager()->getSubscriberManager($this->getMandantName());
+
+		$member = $subManager->findOneBy(array('id' => $id));
+
+		if (!$member)
+			throw $this->createNotFoundException(sprintf("Subscriber %d not found in mandant \"%s\"", $id, $this->getMandantName()));
+
+		$em = $this->getDoctrine()->getManager($this->getMandantName());
+		$em->remove($member);
+		$em->flush();
+
+		$this->container->get('session')->getFlashBag()->add(
+			'notice',
+			'subscriber.delete.status_' . ($subManager->findOneBy(array('id' => $id)) ? 'ko' : 'ok')
+		);
+
+		return $this->redirect($this->generateUrl('ibrows_newsletter_subscriber_list'));
+	}
 }
